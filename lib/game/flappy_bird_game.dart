20c09d8db10fa8a374782e1e3fb635e75ea0b521
Passionate Flutter developer <129858140+RomanDevelop@users.noreply.github.com>
2024-09-28 20:55:06 +0200

    components done

--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -1,3 +1,18 @@
 import 'package:flame/game.dart';
+import 'package:flutter_game_test/components/background.dart';
+import 'package:flutter_game_test/components/bird.dart';
+import 'package:flutter_game_test/components/ground.dart';
+import 'package:flutter_game_test/components/pipe_group.dart';
 
-class FlappyBirdGame extends FlameGame {}
+class FlappyBirdGame extends FlameGame {
+  late Bird bird;
+  @override
+  Future<void> onLoad() async {
+    addAll([
+      Background(),
+      Ground(),
+      bird = Bird(),
+      PipeGroup(),
+    ]);
+  }
+}
